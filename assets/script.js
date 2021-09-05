2e2a17094601ba730e9c47f8383c2fd1d9faabe3
Ben Kaan <benkaan001@gmail.com>
2021-09-04 23:57:17 -0500

    question div HTML and JS format

--- assets/script.js
+++ assets/script.js
@@ -1,81 +1,49 @@
 // list variables here //
 
-var buttonEl = document.querySelector("#start-button");
-
-var startQuiz = function(){
-
-};
-
-//event listeners//
-
-// buttonEl.addEventListener("click", startQuiz);
 
 // questions //
 
 var quizQuestions = [
     {
     question: "For arrays, the typeof operator in JavaScript will return... ",
-    optionA: "string",
-    optionB: "number",
-    optionC: "object",
-    optionD: "boolean",
-    correctAnswer: "c"
+    choices:  ["(a) string", "(b) number", "(c) object", "(d) boolean"],
+    correctAnswer: "(c)"
 },
   {
     question: "JavaScript stores dates as ...",
-    optionA: "seconds",
-    optionB: "miliseconds",
-    optionC: "nanoseconds",
-    optionD: "minimiliseconds",
-    correctAnswer: "b"
+    choices: ["(a) seconds", "(b) miliseconds", "(c) nanoseconds", "(d) minimiliseconds"],
+    correctAnswer: "(b)"
 },
    {
     question: "Math.PI will return...",
-    optionA: "3.14",
-    optionB: "3.154",
-    optionC: "3.314",
-    optionD: "3.41",
-    correctAnswer: "a"
+    choices: [ "(a) 3.14", "(b) 3.154", " (c) 3.314", "(d) 3.41"],
+    correctAnswer: "(a)"
 },
     {
     question: "In JavaScript, a variable without a value has the value...",
-    optionA: "null",
-    optionB: "none",
-    optionC: "undefined",
-    optionD: "false",
-    correctAnswer: "c"
+    choices: [ "(a) null", "(b) none", "(c) undefined", "(d) false"],
+    correctAnswer: "(c)"
 },
     {
     question: "What does JSON stand for?",
-    optionA: "JavaScript Object Norm",
-    optionB: "JavaScript Objective Notation",
-    optionC: "JavScript Order Notation",
-    optionD: "JavaScript Object Notation",
-    correctAnswer: "d"
+    choices: [ "(a) JavaScript Object Norm", "(b) JavaScript Objective Notation", 
+    "(c) JavScript Order Notation", "(d) JavaScript Object Notation"],
+    correctAnswer: "(d)"
 },  
     {
     question: "Which one of the following is NOT a way to declare a JS variable?",
-    optionA: "var",
-    optionB: "let",
-    optionC: "cont",
-    optionD: "const",
-    correctAnswer: "c"
+    choices: [ "(a) var", "(b) let", "(c) cont", "(d) const" ],
+    correctAnswer: "(c)"
 },
     {
     question: "Which one of the following is NOT a JS Keyword?",
-    optionA: "if",
-    optionB: "var",
-    optionC: "for",
-    optionD: "aim",
-    correctAnswer: "d"
+    choices: ["(a) if", "(b) var", "(c) for", "(d) aim"],
+    correctAnswer: "(d)"
 },
 
     {
     question: "When adding a number and a string, JS will treat the number as a(an)...?",
-    optionA: "string",
-    optionB: "number",
-    optionC: "object",
-    optionD: "boolean",
-    correctAnswer: "a"
+    choices: [ "(a)string", "(b) number", "(c) object", "(d) boolean"],
+    correctAnswer: "(a)"
 },
      ];

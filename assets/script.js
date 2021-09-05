79028888479166346c00a0cf5b95753b343513de
Ben Kaan <benkaan001@gmail.com>
2021-09-04 20:58:16 -0500

    add questions and options to JS

---
+++ assets/script.js
@@ -0,0 +1,71 @@
+
+
+// questions //
+
+var quizQuestions = [
+    {
+    question: "For arrays, the typeof operator in JavaScript will return... ",
+    choiceA: "string",
+    choiceB: "number",
+    choiceC: "object",
+    choiceD: "boolean",
+    correctAnswer: "c"
+},
+  {
+    question: "JavaScript stores dates as ...",
+    choiceA: "seconds",
+    choiceB: "miliseconds",
+    choiceC: "nanoseconds",
+    choiceD: "minimiliseconds",
+    correctAnswer: "b"
+},
+   {
+    question: "Math.PI will return...",
+    choiceA: "3.14",
+    choiceB: "3.154",
+    choiceC: "3.314",
+    choiceD: "3.41",
+    correctAnswer: "a"
+},
+    {
+    question: "In JavaScript, a variable without a value has the value...",
+    choiceA: "null",
+    choiceB: "none",
+    choiceC: "undefined",
+    choiceD: "false",
+    correctAnswer: "c"
+},
+    {
+    question: "What does JSON stand for?",
+    choiceA: "JavaScript Object Norm",
+    choiceB: "JavaScript Objective Notation",
+    choiceC: "JavScript Order Notation",
+    choiceD: "JavaScript Object Notation",
+    correctAnswer: "d"
+},  
+    {
+    question: "Which one of the following is NOT a way to declare a JS variable?",
+    choiceA: "var",
+    choiceB: "let",
+    choiceC: "cont",
+    choiceD: "const",
+    correctAnswer: "c"
+},
+    {
+    question: "Which one of the following is NOT a JS Keyword?",
+    choiceA: "if",
+    choiceB: "var",
+    choiceC: "for",
+    choiceD: "aim",
+    correctAnswer: "d"
+},
+
+    {
+    question: "When adding a number and a string, JS will treat the number as a(an)...?",
+    choiceA: "string",
+    choiceB: "number",
+    choiceC: "object",
+    choiceD: "boolean",
+    correctAnswer: "a"
+},
+     ];
